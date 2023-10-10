030358638e7fe53e9404f6600d5fbccc33f9f7f0
rogerviliano <rogrogvil@gmail.com>
2023-10-10 19:50:30 -0300

    feat(for):add exercise on for,keys,values,get with dictionary

---
+++ aulas/aula_29_exercicio_for_dict.py
@@ -0,0 +1,16 @@
+dict_alunos={"aluno":"João","Convidado":"Maria","Professor":"Paulo","Colega":"Abraão","Padre":"Lucas","Amigo":"Gilberto","Metrologista":"Roger"}
+print("\n>>>>>>>>>Chaves:")
+for x in dict_alunos:
+    print(x) #recebe cada chave em x
+    print(dict_alunos.values())#imprime uma lista com os valores
+
+yy=dict_alunos.keys()
+for i in yy:
+    print("Chave: ",i)
+
+
+print("\n>>>>>>>>>>Valores")
+for y in dict_alunos: #y recebe 'key'
+    xx=dict_alunos.get(y)#xx armazerna 'value' de y
+    print(xx) #exibe valor de xx
+print(len(dict_alunos))#comprimento do dicionario
